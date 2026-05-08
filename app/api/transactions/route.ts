8d037a8ccb8a542b9cb7718b372c14717150d8d6
Zyfay Official <zyfayofficial@gmail.com>
2026-05-08 14:03:52 +0700

    Delete app directory

--- app/api/transactions/route.ts
+++
@@ -1,8 +0,0 @@
-// app/api/transactions/route.ts
-import { NextResponse } from "next/server";
-import { getAllTransactions } from "@/lib/store";
-
-export async function GET() {
-  const all = getAllTransactions();
-  return NextResponse.json({ success: true, data: all });
-}
